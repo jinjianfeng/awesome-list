02a6a2d054c0d981860ff0aa4ab6aa74f8a3855e
jinjianfeng <jianfeng.jjf@alibaba-inc.com>
2018-12-19 10:41:21 +0800

    - 更改getUrlParam 方法，将uneascape方法换成decodeURIComponent

--- getUrlParam.js
+++ getUrlParam.js
@@ -3,7 +3,7 @@ function getQueryString(name, url = window.location) {
     var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i')
     var r = url.search.substr(1).match(reg)
     if (r != null) {
-        return unescape(r[2])
+        return decodeURIComponent(r[2])
     }
     return ''
 }
